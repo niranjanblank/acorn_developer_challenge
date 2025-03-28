f19e20b373a4da7bbda4aca24d461e98471c9de5
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-28 17:37:25 +1100

    feat(backend_models): abstract base class called CatalogueContent created which will be inherited by Course and LiveLearning

---
+++ app/ExternalCatalogue/CatalogueContent.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\ExternalCatalogue;
+
+abstract class CatalogueContent
+{
+
+    protected array $data;
+
+    public function __construct(array $data){
+        $this -> data = $data;
+    }
+
+    // these base fields will be in each child of this class(e.g LiveLearning and Course)
+    protected function baseFields(): array{
+        return [
+            'fullname' => $this->data['fullname'] ?? '',
+            'summary' => $this->data['summary'] ?? '',
+            'summaryText' => $this->data['summarytext'] ?? '',
+            'contentType' => $this->data['contenttype'] ?? 'unknown',
+            'url' => $this->data['url'] ?? '',
+            'imageUrl' => $this->data['imageurl'] ?? '',
+            'completionstatus' => $this->data['completionstatus'] ?? '',
+            'cost' => $this->data['cost']?? 0,
+            'timeCreated' => $this->data['timecreated'] ?? '',
+            'timeModified' => $this->data['timemodified'] ?? ''
+        ];
+    }
+
+    // this function needs to be implented by the child classes so they have the common data and their type specific data
+    abstract public function toArray(): array;
+}
