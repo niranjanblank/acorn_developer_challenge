77bcb9dca744ec4e8cb10ed63be2e86b1a3886f9
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-28 20:26:00 +1100

    refactor(routes): Unnecesary routes removed

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,16 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ExternalCatalogueController;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
+// route for the api to call external catalogue api
 Route::get('/api/catalogue', [ExternalCatalogueController::class, 'index']);
 
-Route::get('/catalogue', function () {
+// route to render react app catalogue
+Route::get('/', function () {
     return Inertia::render('catalogue');
 });
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
